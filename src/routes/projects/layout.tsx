f9c0f548a2260f764d2a1ef875ae63b325ecd8c7
Jack Dunn <jmhjdunn123@outlook.com>
2026-09-14 16:09:39 -0400

    Another mobile fix

--- src/routes/projects/layout.tsx
+++ src/routes/projects/layout.tsx
@@ -2,7 +2,7 @@ import { Slot, component$ } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
-      <main class="lg:col-span-8 space-y-4 prose prose-neutral !mx-auto lg:!m-0 lg:max-w-none">
+      <main class="lg:col-span-8 space-y-4 prose prose-neutral lg:!m-0 mx-auto lg:max-w-none">
         <Slot />
       </main>
   );
