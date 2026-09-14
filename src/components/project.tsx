a95af6bf7e42f75646a2880062c43a3022bde647
Jack Dunn <jmhjdunn123@outlook.com>
2026-09-14 18:12:01 -0400

    Styling changes

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,40 +1,64 @@
 import { component$ } from "@builder.io/qwik";
-import { BsArrowRight } from "@qwikest/icons/bootstrap";
 
 interface ProjectProps {
-	title: string,
-	description: string,
-	link?: string,
-	github?: string,
-	tools?: any[]
+	title: string;
+	description: string;
+	link?: string;
+	github?: string;
+	tools?: any[];
 }
 
 interface ProjectTagProps {
-	title: string	
-	icon?: any
+	title: string;
+	icon?: any;
 }
 
-export const ProjectTag = component$(({title, icon, }: ProjectTagProps) => {
+export const ProjectTag = component$(({ title, icon }: ProjectTagProps) => {
+	return (
+		<span class="inline-flex items-center gap-1 rounded border border-neutral-200 bg-neutral-100 px-2 py-1 text-xs font-medium text-neutral-700">
+			{typeof icon !== "undefined" && icon}
+			{title}
+		</span>
+	);
+});
 
-	return <span  class="inline-flex items-center gap-1 px-2 py-1 rounded bg-neutral-100 text-neutral-700 text-sm font-medium border border-neutral-200">
-		{typeof icon !== "undefined" && icon}
-		{title}
-	</span>
-})
+export default component$(
+	({ title, description, link, tools, github }: ProjectProps) => {
+		return (
+			<article class="space-y-4 border-b border-neutral-200 pb-8">
+				{typeof link !== "undefined" ? (
+					<a class="text-lg text-neutral-900" href={`/projects/${link}`}>
+						{title}
+					</a>
+				) : (
+					<h3 class="text-lg text-neutral-900">{title}</h3>
+				)}
 
-export default component$(({title, description, link, tools, github}: ProjectProps) => {
-	return (<article class="space-y-2 pb-4 border-b border-neutral-200">
-          {typeof link !== "undefined" ? <a class="text-lg text-neutral-900 underline" href={`/projects/${link}`}>{title}</a> : <h3 class="text-lg text-neutral-900">{title}</h3>}
-
-          <p class="text-sm leading-releaxed text-neutral-600">{description}</p>
-          {typeof github !== "undefined" && <a href={`https://github.com/jdunn99/${github}`} class="text-sm leading-releaxed text-blue-600 inline-flex gap-1 items-center" target="_blank">View on GitHub <BsArrowRight /></a>}
-          <div class="flex items-center gap-2 text-xs pt-2">
-          	{typeof tools !== "undefined" && tools.map((tool) => (
-          		<ProjectTag key={tool.title} {...tool} />
-      		))}
-      {/*    	{test.map((t) => (
-      		))}
-*/}            {/*TODO Component*/}
-          </div>
-        </article>)
-})
+				<div class="flex items-center gap-2 pt-2 text-xs">
+					{typeof tools !== "undefined" &&
+						tools.map((tool) => <ProjectTag key={tool.title} {...tool} />)}
+				</div>
+				<p class="leading-releaxed text-sm text-neutral-600">{description}</p>
+				<div class="flex items-center gap-2">
+					{typeof link !== "undefined" ? (
+						<a
+							href={`/projects/${link}`}
+							class="rounded-lg bg-sky-800 p-1.5 text-sm text-white"
+						>
+							Read More
+						</a>
+					) : null}
+					{typeof github !== "undefined" && (
+						<a
+							href={`https://github.com/jdunn99/${github}`}
+							class="leading-releaxed inline-flex items-center gap-1 rounded-lg border border-neutral-300 bg-neutral-100 p-1.5 text-sm text-neutral-600"
+							target="_blank"
+						>
+							View on GitHub{" "}
+						</a>
+					)}
+				</div>
+			</article>
+		);
+	},
+);
--- src/projects.tsx
+++ src/projects.tsx
@@ -1,68 +1,65 @@
-import Excel from './components/icons/Excel'
-import JavaScript from './components/icons/JavaScript'
-import Matlab from './components/icons/Matlab'
-import Python from './components/icons/Python'
-import Wix from './components/icons/Wix'
+import Excel from "./components/icons/Excel";
+// import JavaScript from "./components/icons/JavaScript";
+import Matlab from "./components/icons/Matlab";
+import Python from "./components/icons/Python";
+// import Wix from "./components/icons/Wix";
 
 const PROJECTS = [
 	{
 		title: "Data Encryption Standard (DES) in Excel VBA",
 		link: "des",
 		github: "DES-in-VBA",
-		description: "Created a custom VBA class to simulate bit operations, enabling spreadsheet encryption via DES. Built functions including 16-round subkey generation, expansion functions, S-Box substitutions, and file parsing modules for full encryption and decryption.",
+		description:
+			"Created a custom VBA class to simulate bit operations, enabling spreadsheet encryption via DES. Built functions including 16-round subkey generation, expansion functions, S-Box substitutions, and file parsing modules for full encryption and decryption.",
 		tools: [
 			{
 				title: "Excel",
-				icon: <Excel />
+				icon: <Excel />,
 			},
 			{
 				title: "VBA",
 			},
-			{
-				title: "Cryptography"
-			}
 		],
-		images: ["/credit_risk/default_rates.png", "/credit_risk/transition_matrix.png", "/credit_risk/steady_state.png"]
+		images: [
+			"/credit_risk/default_rates.png",
+			"/credit_risk/transition_matrix.png",
+			"/credit_risk/steady_state.png",
+		],
 	},
 	{
 		title: "Credit Risk Analysis of a Corporate Bond Portfolio",
 		link: "credit-risk",
-		description: "Senior capstone mathematical modeling project. Calculated and used forward curves to price bonds across all S&P Global credit rating levels across various time horizons. Modeled and compared cumulative default rates for each rating across a 10-year period, implementing Credit Metrics Monte Carlo simulations with Markov models to forecast portfolio-level credit Value at Risk.",
+		description:
+			"Senior capstone mathematical modeling project. Calculated and used forward curves to price bonds across all S&P Global credit rating levels across various time horizons. Modeled and compared cumulative default rates for each rating across a 10-year period, implementing Credit Metrics Monte Carlo simulations with Markov models to forecast portfolio-level credit Value at Risk.",
 		tools: [
 			{
 				title: "MATLAB",
-				icon: <Matlab />
+				icon: <Matlab />,
 			},
 			{
 				title: "Python",
-				icon: <Python />
+				icon: <Python />,
 			},
-			{
-				title: "S&P Global"
-			},
-		]
+		],
 	},
 	// {
 	// 	title: "Classifciation and Regression Trees in Julia",
 	// 	description: "Coming Soon"
 	// },
-	{
-		title: "Cheryl Zapata Art",
-		description: "Engineered a complete end-to-end web solution for an artist portfolio and e-commerce platform using Wix and custom JavaScript. Developed reusable custom components to elevate user experience and showcase artwork while driving traffic through search engine optimization.",
-		tools: [
-			{
-				title: "Wix",
-				icon: <Wix />
-			},
-			{
-				title: "JavaScript",
-				icon: <JavaScript />
-			},
-			{
-				title: "Google Developer Console"
-			},
-		]
-	}
-]
+	// {
+	// 	title: "Cheryl Zapata Art",
+	// 	description: "Engineered a complete end-to-end web solution for an artist portfolio and e-commerce platform using Wix and custom JavaScript. Developed reusable custom components to elevate user experience and showcase artwork while driving traffic through search engine optimization.",
+	// 	tools: [
+	// 		{
+	// 			title: "Wix",
+	// 			icon: <Wix />
+	// 		},
+	// 		{
+	// 			title: "JavaScript",
+	// 			icon: <JavaScript />
+	// 		},
+	// 	]
+	// }
+];
 
-export default PROJECTS
+export default PROJECTS;
