81df3ec56db546da4bbb8ef85cc8840d04d9d14e
Jack Dunn <jmhjdunn123@outlook.com>
2026-09-19 14:28:31 -0400

    Fixed build error

--- src/components/icons/Julia.tsx
+++ src/components/icons/Julia.tsx
@@ -1,5 +1,5 @@
 import { component$ } from '@builder.io/qwik'
-import Image from './Julia.png?w=18&h=18&jsx';
+import Image from './julia.png?w=18&h=18&jsx';
 
 export default component$(() => {
   return (
